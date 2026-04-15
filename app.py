9cb6f6d7bdd88760a86f6380361f2bab355f9f30
RautelaSahil <sahil.rautela.ssr@gmail.com>
2026-04-16 00:25:40 +0530

    App

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables FIRST
+load_dotenv(override=True)
+
 from flask import Flask
 from db import init_db
 from middleware.request_logger import register_request_logger
@@ -17,11 +23,13 @@ def apply_cors(response):
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     return response
 
+
 @app.route("/options-handler", methods=["OPTIONS"])
 def options_handler():
     return "", 204
 
-# Blueprints 
+
+# Blueprints
 from routes.create import create_bp
 from routes.mock import mock_bp
 from routes.interceptor import interceptor_bp
@@ -35,15 +43,21 @@ app.register_blueprint(mock_bp)
 app.register_blueprint(interceptor_bp)
 app.register_blueprint(namespace_bp)
 app.register_blueprint(logs_bp)
+
 register_request_logger(app)
+
 
 @app.route("/")
 def landing():
     return app.send_static_file("landing.html")
 
+
 @app.route("/app")
 def app_page():
     return app.send_static_file("app.html")
 
+
 if __name__ == "__main__":
+    # Optional debug check (remove later)
+    print("ENV CHECK:", os.environ.get("GROQ_API_KEY"))
     app.run(debug=True)
